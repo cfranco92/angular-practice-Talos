f167b6feb688a961c92ac58de2a2bd6e71713c4b
Cristian Franco Bedoya <cfrancobedoya@gmail.com>
2021-03-24 15:29:06 -0500

    Creating modal view base.

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -4,11 +4,13 @@ import { PokemonComponent } from './components/pokemon/pokemon.component';
 import { PokemonsComponent } from './components/pokemons/pokemons.component';
 import { SharedModule } from '../shared/shared.module';
 import { PokemonRoutingModule } from './pokemon-routing.module';
+import { ModalViewComponent } from './components/modal-view/modal-view.component';
 
 @NgModule({
   declarations: [
     PokemonComponent,
-    PokemonsComponent
+    PokemonsComponent,
+    ModalViewComponent
   ],
   imports: [
     CommonModule,
