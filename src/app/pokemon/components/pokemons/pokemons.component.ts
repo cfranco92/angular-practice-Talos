68f7efcaf1d594a64edfcdfd5b04ea1e99e3338c
Cristian Franco Bedoya <cfrancobedoya@gmail.com>
2021-03-23 22:07:07 -0500

    Creating pokemon card and first list.

--- src/app/pokemon/components/pokemons/pokemons.component.ts
+++ src/app/pokemon/components/pokemons/pokemons.component.ts
@@ -6,7 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokemons.component.css']
 })
 export class PokemonsComponent implements OnInit {
-
+  pokemons: any = [
+    {
+      name: 'Bulbasaur',
+      image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
+    },
+    {
+      name: 'Ivysaur',
+      image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png'
+    },
+    {
+      name: 'Venusaur',
+      image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png'
+    },
+    {
+      name: 'Charmander',
+      image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png'
+    },
+    {
+      name: 'Charmeleon',
+      image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png'
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
