ca6d3fd0ab1d2508528e4f9f32be7231f7bb28b1
Cristian Franco Bedoya <cfrancobedoya@gmail.com>
2021-03-30 18:18:58 -0500

    Fixing navbar sticky position.

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,21 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatCardModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule
   ],
   exports: [
     MatCardModule,
     FlexLayoutModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule
   ]
 })
 export class MaterialModule { }
